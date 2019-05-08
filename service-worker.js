40780e8182a37ce52bd49b634ea128e2c6e3a1d4
Joshua Whiting <J.Whiting-2018@hull.ac.uk>
2019-05-08 13:01:35 +0100

    Add Service Worker

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const version = "1.02";
+const cacheName = "jw-${version}";
+
+self.addEventListener("install", e => {
+	e.waitUntil(
+		caches.open(cacheName).then(cache => {
+			return cache.addAll([
+				"/",
+				"/index.html",
+				"/manifest.json",
+				"/robots.txt",
+				"/1/index.html",
+				"/2/index.html",
+				"/3/index.html",
+				"/4/index.html",
+				"/5/index.html",
+				"/6/index.html",
+				"/css/button.css",
+				"/css/color.css",
+				"/css/font.css",
+				"/css/list.css",
+				"/css/main.css",
+				"/css/reset.css",
+				"https://fonts.googleapis.com/css?family=Roboto",
+				"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu72xKOzY.woff2",
+				"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxK.woff2",
+				"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxM.woff",
+				"https://fonts.googleapis.com/icon?family=Material+Icons",
+				"https://fonts.gstatic.com/s/materialicons/v43/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2",
+				"https://fonts.gstatic.com/s/materialicons/v43/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.woff",
+				"images/logo/logo-192.png",
+				"images/logo/logo-192.svg",
+				"images/logo/logo-512.png",
+				"images/logo/logo-512.svg"
+			])
+			.then(() => self.skipWaiting());
+		})
+	);
+});
+
+self.addEventListener("activate", event => {
+	event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.open(cacheName)
+		.then(cache => cache.match(event.request, {ignoreSearch: true}))
+		.then(response => {
+			return response || fetch(event.request);
+		})
+	);
+});
