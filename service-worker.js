c0d65e4e50d3b6940cbb1e2b908689a656f187b4
Joshua Whiting <J.Whiting-2018@hull.ac.uk>
2019-05-08 13:27:27 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,36 @@
-const version = "1.00";
+const version = "1.01";
 const cacheName = "ProjectEuler-${version}";
 
 self.addEventListener("install", e => {
 	e.waitUntil(
 		caches.open(cacheName).then(cache => {
 			return cache.addAll([
-				"/index.html"
+				"/",
+				"/index.html",
+				"/manifest.json",
+				"/1/index.html",
+				"/2/index.html",
+				"/3/index.html",
+				"/4/index.html",
+				"/5/index.html",
+				"/6/index.html",
+				"/css/button.css",
+				"/css/color.css",
+				"/css/font.css",
+				"/css/list.css",
+				"/css/main.css",
+				"/css/reset.css",
+				"https://fonts.googleapis.com/css?family=Roboto",
+				"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu72xKOzY.woff2",
+				"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxK.woff2",
+				"https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxM.woff",
+				"https://fonts.googleapis.com/icon?family=Material+Icons",
+				"https://fonts.gstatic.com/s/materialicons/v43/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2",
+				"https://fonts.gstatic.com/s/materialicons/v43/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.woff",
+				"images/logo/logo-192.png",
+				"images/logo/logo-192.svg",
+				"images/logo/logo-512.png",
+				"images/logo/logo-512.svg"
 			])
 			.then(() => self.skipWaiting());
 		})
